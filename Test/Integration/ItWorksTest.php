80b9249f02c08667a626d134a983e1fc266bfeef
Damien Retzinger <damienwebdev@gmail.com>
2023-01-31 22:46:56 -0500

    feat(QuoteGraphQl): support "customer_address_id".
    This should probably be a uid, but ya know, Magento.

---
+++ Test/Integration/ItWorksTest.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+namespace Graycore\QuoteGraphQlAux\Test\Integration;
+
+class ItWorksTest extends \PHPUnit\Framework\TestCase
+{
+
+    public function testItWorks(): void
+    {
+        $this->assertEquals(true, true);
+    }
+}
